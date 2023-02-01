2025888192ca1431dfdaba33cb20bb7354f48f54
Jared Coleman <jaredraycoleman@gmail.com>
2023-01-31 16:18:05 -0800

    Adds weather client

---
+++ weather_client.py
@@ -0,0 +1,23 @@
+import requests
+from typing import Dict
+
+# connect to a "real" API
+
+## Example: OpenWeatherMap
+URL = "https://api.openweathermap.org/data/2.5/weather"
+
+# TODO: get an API key from openweathermap.org and fill it in here!
+API_KEY = ""
+
+def get_weather(city) -> Dict:
+    res = requests.get(URL, params={"q": city, "appid": API_KEY})
+    return res.json()
+
+# TODO: try connecting to a another API! e.g. reddit (https://www.reddit.com/dev/api/)
+
+def main():
+    temp = get_weather("London")
+    print(temp)
+
+if __name__ == "__main__":
+    main()
